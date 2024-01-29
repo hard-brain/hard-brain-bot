24cd33317c27c3161f309664dbc64a5fc10ad6f4
Nathan <101812777+corndogit@users.noreply.github.com>
2024-01-29 23:12:21 +0000

    bot listens for message and checks if it is correct answer

--- hard_brain_bot/cogs/quiz_commands.py
+++ hard_brain_bot/cogs/quiz_commands.py
@@ -14,30 +14,34 @@ from hard_brain_bot.utils import http_requests
 class QuizCommands(commands.Cog):
     def __init__(self, bot: HardBrain):
         self.bot = bot
+        self.round_in_progress = False
+        self.current_round: dict | None = None
+        self.voice: VoiceClient | None = None
 
-    @commands.slash_command(description="say hello")
-    async def hello(self, ctx: disnake.ApplicationCommandInteraction):
-        await ctx.response.send_message("Hello")
+    @commands.Cog.listener()
+    async def on_message(self, ctx: disnake.Message):
+        if not self.round_in_progress or ctx.author == self.bot.user.id:
+            return
+        answer = set(map(lambda s: s.lower(), (self.current_round["title"], *self.current_round["alt_titles"])))
+        if ctx.content.lower() in answer:
+            await ctx.channel.send(f"Correct! The answer was {self.current_round['title']}")
+            self.round_in_progress = False
+            await self.voice.disconnect()
+            self.voice.cleanup()
 
     @commands.slash_command(description="Starts a quiz")
     async def start_quiz(self, ctx):
-        if ctx.author.id == self.bot.user.id:
-            return
-
         # check user is in voice channel
         if not (connected := ctx.author.voice):
-            return await ctx.channel.send(
-                "Error: you are not connected to a voice channel"
-            )
-
-        await ctx.channel.send(f"Quiz starting in #{connected.channel}!")
-
+            await ctx.channel.send("Error: you are not connected to a voice channel")
+            return
         try:
             async with ClientSession() as session:
                 question_response = await http_requests.request_json(
                     "GET", "http://localhost:8000/question", session
                 )
-                song_id = question_response[0]["song_id"]
+                self.current_round = question_response[0]
+                song_id = self.current_round["song_id"]
                 audio_response = await http_requests.request_bytes(
                     "GET", f"http://localhost:8000/audio/{song_id}", session
                 )
@@ -46,10 +50,12 @@ class QuizCommands(commands.Cog):
             await ctx.channel.send(f"Error occurred while fetching song response...")
             return
 
+        await ctx.channel.send(f"Quiz starting in #{connected.channel}!")
+        self.round_in_progress = True
         song_bytes = io.BytesIO(audio_response)
         stream = FFmpegPCMAudio(song_bytes, pipe=True)
-        voice: VoiceClient = await connected.channel.connect()
-        voice.play(stream)
+        self.voice = await connected.channel.connect()
+        self.voice.play(stream)
 
 
 def setup(bot: HardBrain):
