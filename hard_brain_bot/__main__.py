f4272f7df2a3a82ae82eb4367b27613c96d3dd46
Nathan <101812777+corndogit@users.noreply.github.com>
2024-02-07 20:58:35 +0000

    remove python-dotenv and update packages

--- hard_brain_bot/__main__.py
+++ hard_brain_bot/__main__.py
@@ -1,23 +1,22 @@
 import os
 
-import dotenv
-
-from client import HardBrain
+from hard_brain_bot.client import HardBrain
 from hard_brain_bot.utils import setup_logging
 
 
 if __name__ == "__main__":
     bot = HardBrain(debug=True)
-    bot.load_extension("cogs.quiz_commands")
+    bot.load_extension("hard_brain_bot.cogs.quiz_commands")
 
     @bot.event
     async def on_ready():
         print(f"Logged in as {bot.user} (ID: {bot.user.id})")
 
-    dotenv.load_dotenv("../.env")
     setup_logging(
         "disnake",
-        os.path.abspath("../discord.log"),
+        os.path.abspath("discord.log"),
         "%(asctime)s:%(levelname)s:%(name)s: %(message)s",
     )
-    bot.run(os.getenv("DISCORD_TOKEN"))
+    if not (token := os.getenv("DISCORD_TOKEN")):
+        raise RuntimeError("DISCORD_TOKEN environment variable is not set.")
+    bot.run(token)
