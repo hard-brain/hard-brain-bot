3adadb4ef02e35522aca433cfd8b81d0faab784b
Nathan <101812777+corndogit@users.noreply.github.com>
2024-02-14 15:14:34 +0000

    add option for specifying more than 1 song

--- hard_brain_bot/services/hard_brain_service.py
+++ hard_brain_bot/services/hard_brain_service.py
@@ -21,11 +21,13 @@ class HardBrainService:
         self.use_https = use_https
         self.url = self.__set_url()
 
-    async def get_question(self, params: dict | None = None) -> dict:
+    async def get_question(self, number_of_songs: int = 1) -> dict:
+        if number_of_songs <= 0:
+            raise ValueError("Number of songs requested must be greater than 0")
         async with ClientSession() as session:
-            if params and (params != {}):
+            if number_of_songs != 1:
                 return await http_requests.request_json(
-                    "GET", f"{self.url}/question", session, params=params
+                    "GET", f"{self.url}/question", session, params={"number_of_songs": number_of_songs}
                 )
             return await http_requests.request_json(
                 "GET", f"{self.url}/question", session
