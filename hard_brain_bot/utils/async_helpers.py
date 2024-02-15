a60b1451584e2d5ffa613622f48e1cb00e8ed39b
Nathan <101812777+corndogit@users.noreply.github.com>
2024-02-15 00:10:37 +0000

    make CancellableTask class and some other changes

--- hard_brain_bot/utils/async_helpers.py
+++ hard_brain_bot/utils/async_helpers.py
@@ -3,17 +3,17 @@ from typing import Callable
 
 
 class AsyncTimer:
-    def __init__(self, timeout: float, callback: Callable, *args, **kwargs) -> None:
+    def __init__(self, timer_timeout: float, callback: Callable, *args, **kwargs) -> None:
         """
         A simple timeout object that imitates `threading.Timer()` while supporting async callbacks.
-        :param timeout: Timeout time to wait in seconds
+        :param timer_timeout: Timeout time to wait in seconds
         :param callback: Function or coroutine to be called after timeout.
         """
         self.loop = asyncio.get_event_loop()
         self.callback = callback
         self.args = args
         self.kwargs = kwargs
-        self.timeout = timeout
+        self.timer_timeout = timer_timeout
         self.task: asyncio.Task | None = None
         self._is_executed = False
 
@@ -21,20 +21,44 @@ class AsyncTimer:
         return isinstance(self.task, asyncio.Task)
 
     async def _job(self) -> None:
-        await asyncio.sleep(self.timeout)
+        await asyncio.sleep(self.timer_timeout)
         await self.callback(*self.args, **self.kwargs)
         self._is_executed = True
 
-    def start(self):
-        if self._is_executed:
-            raise RuntimeError("Tried to start a finished AsyncTimer")
+    def start(self) -> None:
         if self._is_started():
             raise RuntimeError("Tried to start an in-progress AsyncTimer")
+        if self.task.done():
+            raise RuntimeError("Tried to start a finished AsyncTimer")
         self.task = self.loop.create_task(self._job())
 
-    def cancel(self):
-        if self._is_executed:
-            raise RuntimeError("Tried to cancel a finished AsyncTimer")
+    async def timeout(self) -> None:
+        try:
+            await self.task.result()
+        except asyncio.CancelledError | asyncio.InvalidStateError:
+            # means the task is complete anyway
+            pass
+
+    def cancel(self) -> None:
         if not self._is_started():
-            raise RuntimeError("Tried to cancel a AsyncTimer that is not running a task")
+            raise RuntimeError(
+                "Tried to cancel a AsyncTimer that is not running a task"
+            )
+        if self.task.done():
+            raise RuntimeError("Tried to cancel a finished AsyncTimer")
+        self.task.cancel()
+
+
+class CancellableTask:
+    def __init__(self, callback: Callable, *args, **kwargs) -> None:
+        self.loop = asyncio.get_event_loop()
+        self.callback = callback
+        self.args = args
+        self.kwargs = kwargs
+        self.task: asyncio.Task | None = None
+
+    def start(self) -> None:
+        self.task = self.loop.create_task(self.callback(*self.args, **self.kwargs))
+
+    def cancel(self) -> None:
         self.task.cancel()
