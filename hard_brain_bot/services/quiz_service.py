7e890ad6067c3afdd70f405235990fe3d16fccad
Nathan <101812777+corndogit@users.noreply.github.com>
2024-02-15 20:54:53 +0000

    wip: progress towards multiple round quizzes

--- hard_brain_bot/services/quiz_service.py
+++ hard_brain_bot/services/quiz_service.py
@@ -3,11 +3,12 @@ import io
 import platform
 import logging
 from aiohttp import ClientError
+import asyncio
 
-from disnake import FFmpegPCMAudio, ApplicationCommandInteraction
+from disnake import FFmpegOpusAudio, ApplicationCommandInteraction
 
 from hard_brain_bot.data_models.requests import SongData
-from hard_brain_bot.utils.async_helpers import AsyncTimer, CancellableTask
+from hard_brain_bot.utils.async_helpers import AsyncTimer
 from hard_brain_bot.services.hard_brain_service import HardBrainService
 from hard_brain_bot.message_templates.embeds import embed_song_data
 
@@ -38,40 +39,39 @@ class QuizService:
         self.followup = ctx.followup
         self.voice_channel = ctx.author.voice.channel
         self._game_in_progress = False
-        self._current_round = 0
+        self._current_round = 1
+        self._current_song: SongData | None = None
         self._round_timer: AsyncTimer | None = None
         self._voice: disnake.VoiceClient | None = None
-        self._stream: disnake.FFmpegPCMAudio | None = None
-        self._game: CancellableTask | None = None
+        self._stream: FFmpegOpusAudio | None = None
+        self._queue = asyncio.Queue()
+
+    async def _process_rounds(self):
+        for song in self.song_data_list:
+            print(song)
+            await self._queue.put(self._next_round(song.song_id))  # todo: blocked on this
 
     async def start_game(self):
         logging.info(
             f"Starting new game with {len(self.song_data_list)} questions in {self.voice_channel}"
         )
-
-        async def _process_rounds():
-            for song in self.song_data_list:
-                await self._next_round(song.song_id)
-
-        self._game = CancellableTask(_process_rounds)
+        self._voice = await self.voice_channel.connect()
+        self._game_in_progress = True
+        await self._process_rounds()
 
     async def _next_round(self, song_id: int):
         try:
             audio_response = await self.backend.get_audio(song_id)
         except ClientError as e:
             logging.error(f"Fetching audio for song id {song_id} failed: {e}")
-            self.followup.send(
-                "An error occurred while loading the next song..."
-            )
+            self.followup.send("An error occurred while loading the next song...")
             await self._end_round()
             return
         song_bytes = io.BytesIO(audio_response)
-        self._stream = FFmpegPCMAudio(song_bytes, pipe=True)
-        self._voice = await self.voice_channel.connect()
+        self._stream = FFmpegOpusAudio(song_bytes, pipe=True)
         self._voice.play(self._stream)
         self._round_timer = AsyncTimer(self.round_time_limit, self._end_round)
         self._round_timer.start()
-        await self._round_timer.timeout()
 
     async def _end_round(self):
         embed = embed_song_data(
@@ -82,7 +82,7 @@ class QuizService:
         await self.followup.send(embed=embed)
         if self._voice and self._voice.is_playing():
             self._voice.stop()
-        if self._stream:
+        if isinstance(self._stream, FFmpegOpusAudio):
             self._stream.cleanup()
         self._current_round += 1
 
@@ -103,9 +103,8 @@ class QuizService:
         await self.followup.send("Game ending. Thank you for playing!")
 
         # clean up game stuff
-        self._game.cancel()
         self._game_in_progress = False
-        self._current_round = 0
+        self._current_round = 1
 
         # clean up voice
         if self._voice:
@@ -113,7 +112,7 @@ class QuizService:
             self._voice.cleanup()
 
     async def skip_round(self):
-        logging.debug(
+        logging.info(
             f"Skipping round {self._current_round} out of {len(self.song_data_list)}..."
         )
         await self.followup.send("Skipping round...")
