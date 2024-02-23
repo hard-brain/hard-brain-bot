c12438200c1adbf9253e3e81926dbeeeeb26673b
Nathan <101812777+corndogit@users.noreply.github.com>
2024-02-23 16:51:39 +0000

    update documentation

--- hard_brain_bot/utils/async_helpers.py
+++ hard_brain_bot/utils/async_helpers.py
@@ -1,15 +1,17 @@
 import asyncio
-from typing import Callable
+from typing import Callable, Any
 
 
 class AsyncTimer:
     def __init__(
-        self, timer_timeout: float, callback: Callable, *args, **kwargs
+        self, timer_timeout: float, callback: Callable, *args: Any, **kwargs: Any
     ) -> None:
         """
         A simple timeout object that imitates `threading.Timer()` while supporting async callbacks.
         :param timer_timeout: Timeout time to wait in seconds
         :param callback: Function or coroutine to be called after timeout.
+        :param args: Arguments for the callback.
+        :param kwargs: Keyword arguments for the callback.
         """
         self.loop = asyncio.get_event_loop()
         self.callback = callback
@@ -21,9 +23,17 @@ class AsyncTimer:
         self._event = asyncio.Event()
 
     def is_started(self) -> bool:
+        """
+        Checks if a task has been created and started in a thread loop. Not asynchronous.
+        :return: True or false
+        """
         return isinstance(self.task, asyncio.Task)
 
     def is_executed(self) -> bool:
+        """
+        Checks if the event has been set yet. Not asynchronous.
+        :return:
+        """
         return self._event.is_set()
 
     async def _job(self) -> None:
@@ -33,6 +43,9 @@ class AsyncTimer:
         self._event.set()
 
     def start(self) -> None:
+        """
+        Starts the timer with the provided callback and timeout duration. Not asynchronous.
+        """
         if self.is_started():
             raise RuntimeWarning("Tried to start an in-progress AsyncTimer")
         if self.task and self.task.done():
@@ -40,6 +53,10 @@ class AsyncTimer:
         self.task = self.loop.create_task(self._job())
 
     def cancel(self) -> None:
+        """
+        Cancels the running task and sets the event, signalling to any processes awaiting this timer to release.
+        Not asynchronous.
+        """
         if not self.is_started():
             raise RuntimeWarning(
                 "Tried to cancel a AsyncTimer that is not running a task"
@@ -50,4 +67,7 @@ class AsyncTimer:
         self._event.set()
 
     async def timeout(self) -> None:
+        """
+        Awaitable method that blocks the thread until the event is set, either by cancelling or timing out.
+        """
         await self._event.wait()
